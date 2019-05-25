071460f6fbeee6c60e21e9103eb694b5f05ff88c
erhunt <erhunt@taltech.ee>
2019-05-26 00:23:43 +0300

    Add line 5 webapp.model.credit update "Public interface"

--- src/main/java/com/bootstrapwithspringboot/webapp/repository/Creditrepository.java
+++ src/main/java/com/bootstrapwithspringboot/webapp/repository/Creditrepository.java
@@ -2,8 +2,9 @@ package com.bootstrapwithspringboot.webapp.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import com.bootstrapwithspringboot.webapp.model.credit;
 
 @Repository
-public interface Creditrepository extends CrudRepository {
+public interface Creditrepository extends CrudRepository<credit, Integer> {
 
 }
